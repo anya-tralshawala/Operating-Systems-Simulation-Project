eb1ef54506e6e144c45786ca01f222793a1b455c
keerti-sundaram <70778626+keerti-sundaram@users.noreply.github.com>
2021-07-24 13:27:37 -0400

    Update process.h

--- process.h
+++ process.h
@@ -5,27 +5,36 @@
 class process
 {
 public:
-    process(int aState, int aPid, double aTime,std::vector<cpuBurst> aBurstList);
+    process(int aState, int aPid, double aTime,std::vector<cpuBurst> aBurstList, int atotal_bursts, int abursts_completed);
+    process();
     // ACCESSORS
     int getState() { return state; }
     int getpID(){return pid;}
     int getArrivialTime() { return arrival_time; }
-
+    int getTotalBursts(){ return total_bursts;}
+    int getBurstsCompleted(){ return bursts_completed;}
+    int getRemainingBursts(){ return total_bursts - bursts_completed;}
+    cpuBurst getCurrent(){return currently_on;}
+    std::vector<cpuBurst> getAllBursts(){return all_bursts;}
     // cpuBurst cpuBurst::getBurstAt(int i){return all_bursts[i]}
 
     // MODIFIERS
     void setWaitTime(double set_time){wait_time = set_time;}
     void setTurnaroundTime(double set_time){turnaround_time = set_time;}
     void setCompletionTime(double set_time){completion_time = set_time;}
+    void removeBurst(cpuBurst removethis);
     //private member variables
 private:
     std::vector<cpuBurst> all_bursts;
+    cpuBurst currently_on;
     int state;
     double arrival_time;
     int pid;
     double wait_time;
     double turnaround_time;
     double completion_time;
+    int bursts_completed;
+    int total_bursts;
 
 };
 //bools we will use in sorting
