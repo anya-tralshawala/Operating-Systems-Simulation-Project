0175b30a4f3aacd556047764b14c5130f9b7d62a
keerti-sundaram <70778626+keerti-sundaram@users.noreply.github.com>
2021-07-24 13:25:41 -0400

    Update cpuBurst.cpp

--- cpuBurst.cpp
+++ cpuBurst.cpp
@@ -6,6 +6,10 @@ cpuBurst::cpuBurst(double aCPUT,double anIOTime)
     CPU_time = aCPUT;
     IO_time = anIOTime;
 }
+cpuBurst::cpuBurst(){
+    CPU_time = 0;
+    IO_time = 0;
+}
 
 // bool lesort_by()
 // {
